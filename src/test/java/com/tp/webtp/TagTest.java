027fe4729c3363cd5bab2c7c5d072a9fde867ed2
locky12 <francois.letterle@gmail.com>
2020-10-22 15:37:04 +0200

    add Model

---
+++ src/test/java/com/tp/webtp/TagTest.java
@@ -0,0 +1,36 @@
+package com.tp.webtp;
+
+import com.tp.webtp.model.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TagTest {
+
+    @Test
+    void cannotBuildWithTagNameEmpty () {
+        assertThrows(IllegalArgumentException.class, () -> new Tag(""));
+    }
+
+    @Test
+    void cannotBuildWithNullTagName () {
+        assertThrows(IllegalArgumentException.class, () -> new Tag(null));
+    }
+
+    @Test
+    void mustBeReturnItsTagName () {
+        assertThat(new Tag("toto").getTagName().equals("toto")).isTrue();
+    }
+
+    @Test
+    void mustBeNewTagName () {
+        Tag tag = new Tag("toto");
+        tag.setTagName("titi");
+        assertThat(tag.getTagName().equals("titi")).isTrue();
+    }
+
+}
+
+
+
