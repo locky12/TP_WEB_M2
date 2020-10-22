0b66e680d2f7f0fe7f0d48128830d70357407a7e
locky12 <francois.letterle@gmail.com>
2020-10-22 14:38:00 +0200

    first commit

---
+++ src/main/java/com/tp/webtp/WebtpApplication.java
@@ -0,0 +1,13 @@
+package com.tp.webtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/webtp/WebtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.webtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
